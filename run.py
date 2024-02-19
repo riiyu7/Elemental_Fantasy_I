1442712bc0d46260b1467960f1853d53a833fe82
Talwinder Singh <talwinder.s.nahal@gmail.com>
2024-02-19 12:59:12 +0000

    Add Psuedocode

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+
+# Start Game Functions
+def main():
+    print(" ---------------------------------")
+    print("| Welcome to Elemental Fantasy I  |")
+    print(" ---------------------------------")
+
+def player_name():
+
+# Menu Functions
+
+def main_menu():
+
+def game_manu():
+
+def rules_menu():
+
+# Rules functions
+
+def show_rules():
+
+if __name__ == "__main__":
+    main()
